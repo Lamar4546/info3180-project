440c8921f70a610a11f98a2d917269c0dd310d25
Hosea Nichols <hoseanichols14@gmail.com>
2026-05-03 14:59:44 -0400

    Created reusable alert and loading components

---
+++ src/components/LoadingSpinner.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="text-center py-4">
+    <div class="spinner-border text-primary" role="status">
+      <span class="visually-hidden">Loading...</span>
+    </div>
+
+    <p v-if="message" class="text-muted mt-2 mb-0">
+      {{ message }}
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LoadingSpinner',
+
+  props: {
+    message: {
+      type: String,
+      default: 'Loading...'
+    }
+  }
+};
+</script>
