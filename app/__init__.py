f228de436476a3fb8e1e1ee5d8cd3b8053c4f09c
Mxlten19 <nhutton110@gmail.com>
2026-05-05 16:42:17 -0500

    fixed the CORS in init.py to allow the production domain

--- app/__init__.py
+++ app/__init__.py
@@ -3,12 +3,24 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_cors import CORS
 from .config import Config
+import os
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
-CORS(app, supports_credentials=True, origins=['http://localhost:5173'])
+
+# Allows localhost
+allowed_origins = [
+    'http://localhost:5173',
+    'http://localhost:5000',
+]
+
+render_url = os.environ.get('RENDER_EXTERNAL_URL')
+if render_url:
+    allowed_origins.append(render_url)
+
+CORS(app, supports_credentials=True, origins=allowed_origins)
 
 from . import views, models
