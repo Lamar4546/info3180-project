a04d782140e6a33cb588f72971655d2d503a1cdc
Mxlten19 <nhutton110@gmail.com>
2026-05-05 16:54:47 -0500

    Det db_url in config.py

--- app/config.py
+++ app/config.py
@@ -1,15 +1,17 @@
 import os
 from dotenv import load_dotenv
 
-load_dotenv()  # load environment variables from .env if it exists.
+load_dotenv()
 
 class Config(object):
     """Base Config Object"""
     DEBUG = False
     SECRET_KEY = os.environ.get('SECRET_KEY', 'Som3$ec5etK*y')
-    UPLOAD_FOLDER = os.environ.get('UPLOAD_FOLDER', os.path.join(os.path.dirname(__file__), 'static', 'uploads')
-    )
+
+    UPLOAD_FOLDER = os.environ.get('UPLOAD_FOLDER',os.path.join(os.path.dirname(__file__), 'static', 'uploads'))
     MAX_CONTENT_LENGTH = 5 * 1024 * 1024  # 5 MB max file size
     ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif', 'webp'}
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '').replace('postgres://', 'postgresql://')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False # This is just here to suppress a warning from SQLAlchemy as it will soon be removed
+
+    _db_url = os.environ.get('DATABASE_URL', 'sqlite:///driftdater.db')
+    SQLALCHEMY_DATABASE_URI = _db_url.replace('postgres://', 'postgresql://')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
